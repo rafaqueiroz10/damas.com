76112e6ee23d5e0100dd3f6fd8ed8cf7c86126f5
Rafael Queiroz Vilaça <rafavilaca13@gmail.com>
2024-10-01 20:38:35 -0300

    Remoção da função carregarTabuleiro, responsabilidade da função script.js

--- js/cria-matriz-tabuleiro.js
+++ js/cria-matriz-tabuleiro.js
@@ -23,24 +23,3 @@ function criarMatrizEl() {
 }
 
 let casasEl = criarMatrizEl();
-
-function carregarTabuleiro() {
-    for(let i = 0; i < casasEl.length; i++) {
-        for(let j = 0; j < casasEl[i].length; j++) {
-            if(i == 3 || i == 4)
-                continue;
-
-            let casaEl = casasEl[i][j];
-            if(casaEl.classList.contains('casa-preta')) {
-                if(i < 3) {
-                    inserirPeca(casaEl, 'escura');
-                }
-                if((i > 4 && i < 8)) {
-                    inserirPeca(casaEl, 'branca');
-                }
-            }
-        }
-    }
-}
-
-//carregarTabuleiro();
